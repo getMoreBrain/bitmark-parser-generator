a27c616cc9145fd3994facd7ed22737c9b59e28b
six5536 <10079653+six5536@users.noreply.github.com>
2025-07-21 15:16:14 +1200

    wip: configbuilder

--- src/info/ConfigBuilder.ts
+++ src/info/ConfigBuilder.ts
@@ -18,11 +18,15 @@ import { PropertyFormat } from '../model/enum/PropertyFormat.ts';
  * Config generation options for the parser
  */
 export interface GenerateConfigOptions {
-  //
+  outputDir?: string;
 }
 
 class ConfigBuilder {
-  public async build(_options?: GenerateConfigOptions): Promise<void> {
+  public async build(options?: GenerateConfigOptions): Promise<void> {
+    const opts: GenerateConfigOptions = Object.assign({}, options);
+
+    await this.buildFlat(opts);
+
     const bitConfigs: (_BitConfig & { bitType: BitTypeType })[] = [];
     const groupConfigs: (_GroupsConfig & { key: string })[] = [];
 
@@ -50,7 +54,7 @@ class ConfigBuilder {
       groupConfigs.push(g2);
     }
 
-    const outputFolder = 'tmp';
+    const outputFolder = opts.outputDir ?? 'assets/config';
     const outputFolderBits = path.join(outputFolder, 'bits');
     const outputFolderGroups = path.join(outputFolder, 'groups');
     fs.ensureDirSync(outputFolderBits);
@@ -402,18 +406,184 @@ class ConfigBuilder {
     await Promise.all(fileWrites);
   }
 
-  public getSupportedBitConfigs(): BitConfig[] {
-    const res: BitConfig[] = [];
+  public async buildFlat(options?: GenerateConfigOptions): Promise<void> {
+    const opts: GenerateConfigOptions = Object.assign({}, options);
+
+    const bitConfigs: BitConfig[] = [];
 
     for (const bt of BitType.values()) {
-      if (bt === BitType._error || bt === BitType._comment) continue;
-
       const bitType = Config.getBitType(bt);
-      const config: BitConfig = Config.getBitConfig(bitType);
-      res.push(config);
+
+      const _bitConfig = Config.getBitConfig(bitType);
+      if (_bitConfig) {
+        bitConfigs.push(_bitConfig);
+      }
     }
 
-    return res;
+    const outputFolder = opts.outputDir ?? 'assets/config';
+    const outputFolderBits = path.join(outputFolder, 'bits_flat');
+    fs.ensureDirSync(outputFolderBits);
+
+    const fileWrites: Promise<void>[] = [];
+
+    // BitConfigs
+    for (const b of bitConfigs) {
+      const inherits = [];
+      const tags = [];
+
+      const tagEntriesTypeOrder = [
+        BitTagType.tag,
+        BitTagType.property,
+        BitTagType.resource,
+        BitTagType.group,
+      ];
+      const tagEntries = Object.entries(b.tags ?? []).sort((a, b) => {
+        const tagA = a[1];
+        const tagB = b[1];
+        const typeOrder =
+          tagEntriesTypeOrder.indexOf(tagA.type) - tagEntriesTypeOrder.indexOf(tagB.type);
+        return typeOrder;
+      });
+
+      for (const [tagKey, tag] of tagEntries) {
+        let tagName = tagKey;
+        let tagKeyPrefix = '';
+        let format = '';
+        let description = '';
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let chain: any = undefined;
+
+        if (tag.type === BitTagType.tag) {
+          const resolvedTag = TAGS[tag.configKey];
+          tagName = resolvedTag.tag;
+
+          if (tagName === '%') {
+            description = 'Item';
+            chain = {
+              key: '%',
+              format,
+              min: tag.minCount,
+              max: tag.maxCount,
+              description: 'Lead',
+              chain: {
+                key: '%',
+                format,
+                min: tag.minCount,
+                max: tag.maxCount,
+                description: 'Page number',
+                chain: {
+                  key: '%',
+                  format,
+                  min: tag.minCount,
+                  max: tag.maxCount,
+                  description: 'Margin number',
+                },
+              },
+            };
+          } else if (tagName === '!') {
+            description = 'Instruction';
+          } else if (tagName === '?') {
+            description = 'Hint';
+          } else if (tagName === '#') {
+            description = 'Title';
+          } else if (tagName === '##') {
+            description = 'Sub-title';
+          } else if (tagName === '▼') {
+            description = 'Anchor';
+          } else if (tagName === '►') {
+            description = 'Reference';
+          } else if (tagName === '$') {
+            description = 'Sample solution';
+          } else if (tagName === '&') {
+            description = 'Resource';
+          } else if (tagName === '+') {
+            description = 'True statement';
+          } else if (tagName === '-') {
+            description = 'False statement';
+          } else if (tagName === '_') {
+            description = 'Gap';
+          } else if (tagName === '=') {
+            description = 'Mark';
+          }
+
+          format = 'bitmark--';
+        } else if (tag.type === BitTagType.property) {
+          const resolvedProperty = PROPERTIES[tag.configKey];
+          tagName = resolvedProperty.tag;
+
+          tagKeyPrefix = '@';
+
+          const property = resolvedProperty as PropertyTagConfig;
+
+          // format
+          if (property.format === PropertyFormat.plainText) {
+            format = 'string';
+          } else if (property.format === PropertyFormat.boolean) {
+            format = 'bool';
+          } else if (property.format === PropertyFormat.bitmarkText) {
+            format = 'bitmark';
+          } else if (property.format === PropertyFormat.number) {
+            format = 'number';
+          }
+        } else if (tag.type === BitTagType.resource) {
+          tagKeyPrefix = '&';
+        } else if (tag.type === BitTagType.group) {
+          tagKeyPrefix = '@';
+          let k = tag.configKey as string;
+          if (k.startsWith('group_')) k = k.substring(6);
+          k = '_' + k;
+          inherits.push(k);
+          continue;
+        }
+
+        const t = {
+          key: tagKeyPrefix + tagName,
+          format,
+          default: null,
+          alwaysInclude: false,
+          min: tag.minCount == null ? 0 : tag.minCount,
+          max: tag.maxCount == null ? 1 : tag.maxCount,
+          description,
+          chain,
+          // raw: {
+          //   ...tag,
+          // },
+        };
+        tags.push(t);
+      }
+
+      const bitJson = {
+        name: b.bitType,
+        description: '',
+        since: b.since,
+        deprecated: b.deprecated,
+        history: [
+          {
+            version: b.since,
+            changes: ['Initial version'],
+          },
+        ],
+        format: b.textFormatDefault ?? 'bitmark--',
+        bodyAllowed: b.bodyAllowed ?? true,
+        bodyRequired: b.bodyRequired ?? false,
+        footerAllowed: b.footerAllowed ?? true,
+        footerRequired: b.footerRequired ?? false,
+        resourceAttachmentAllowed: b.resourceAttachmentAllowed ?? true,
+        tags,
+      };
+
+      const output = path.join(outputFolderBits, `${b.bitType}.jsonc`);
+      const str = JSON.stringify(bitJson, null, 2);
+      fileWrites.push(fs.writeFile(output, str));
+
+      // const bitType = b.bitType;
+      // const bitType2 = Config.getBitType(bitType);
+      // if (bitType !== bitType2) {
+      //   console.log(`BitType: ${bitType} => ${bitType2}`);
+      // }
+    }
+
+    await Promise.all(fileWrites);
   }
 }
 
