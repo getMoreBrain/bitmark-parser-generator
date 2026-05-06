651d5f4c046ec1cffc43050a04ceae66d17c0386
six5536 <10079653+six5536@users.noreply.github.com>
2026-05-06 14:57:27 +1200

    fix: fixed imagePlaceholder config

--- src/config/raw/groups.ts
+++ src/config/raw/groups.ts
@@ -1050,6 +1050,7 @@ const GROUPS: _GroupsConfig = {
     tags: [
       {
         key: ConfigKey.resource_imagePlaceholder,
+        jsonKey: 'imagePlaceholder|resource(type=image, key=image)',
         description: 'Placeholder image for the resource',
         chain: [
           {
